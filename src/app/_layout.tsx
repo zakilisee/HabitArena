936c5f63821ec04d40588303e1cacf76d05e25e5
zakilisee <zakilisee@gmail.com>
2026-07-04 14:27:25 +0100

    edit app to support clerk

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,18 @@
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
 import { Stack } from "expo-router";
 import "../../global.css"
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Stack />
+    </ClerkProvider>
+);
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,44 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useAuth } from '@clerk/expo'
+import { AuthView, UserButton } from '@clerk/expo/native'
+import { useState } from 'react'
+import { View, StyleSheet, ActivityIndicator, Button, Modal } from 'react-native'
 
-export default function Index() {
+export default function MainScreen() {
+  const { isSignedIn, isLoaded } = useAuth({ treatPendingAsSignedOut: false })
+  const [isAuthOpen, setIsAuthOpen] = useState(false)
+
+  if (!isLoaded) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Text className="bg-red-800">Edit src/app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      {isSignedIn ? <UserButton /> : <Button title="Sign in" onPress={() => setIsAuthOpen(true)} />}
+      <Modal
+        animationType="slide"
+        visible={isAuthOpen}
+        presentationStyle="pageSheet"
+        onRequestClose={() => setIsAuthOpen(false)}
+      >
+        <AuthView onDismiss={() => setIsAuthOpen(false)} />
+      </Modal>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
